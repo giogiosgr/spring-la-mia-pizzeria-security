4c5d934a58cd1d6c72a0eaafd1218ae0b0fa97cd
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-09-17 13:48:07 +0200

    fix delete ingredient

--- src/main/java/com/example/demo/model/Ingredient.java
+++ src/main/java/com/example/demo/model/Ingredient.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -43,7 +44,7 @@ public class Ingredient {
 	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 	
-	@ManyToMany(mappedBy = "ingredients")
+	@ManyToMany(mappedBy = "ingredients", cascade = { CascadeType.REMOVE })
 	private List<Pizza> pizzas;
 	
 	// getters e setters
