3f5d0b6df0452e87ead4ac7a092487476b5ea550
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-09-19 20:19:36 +0200

    fix security

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
 	@SuppressWarnings("removal")
@@ -16,8 +18,14 @@ public class SecurityConfiguration {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests().requestMatchers("/user").hasAuthority("USER").requestMatchers("/admin")
-				.hasAuthority("ADMIN").requestMatchers("/").permitAll().and().formLogin().and().logout();
+		http.authorizeHttpRequests()
+		.requestMatchers("/user").hasAuthority("USER")
+		.requestMatchers("/admin").hasAuthority("ADMIN")
+		.requestMatchers("/**").hasAuthority("USER")
+		//.requestMatchers("/pizzas").hasAuthority("USER")
+		//.requestMatchers("/pizzas/edit/*").hasAuthority("ADMIN")
+		//.requestMatchers("/pizzas/create").hasAuthority("ADMIN")
+		.and().formLogin().and().logout();
 
 		return http.build();
 	}
